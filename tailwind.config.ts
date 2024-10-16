08a6fcbe9136676ca62b505e58071f0f15129190
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2024-10-15 20:12:16 -0600

    style: :sparkles: add css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,36 @@ import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+        },
+        border: {
+          DEFAULT: "hsl(var(--border))",
+        },
+        background: {
+          DEFAULT: "hsl(var(--background))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+        },
+      },
+    },
+  },
   plugins: [],
 };
 
